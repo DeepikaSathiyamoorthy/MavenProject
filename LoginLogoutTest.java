c6a4711fb11b74aa8e830a9dc111e90ae4cfa464
DeepikaSathiyamoorthy <dpe.sat@gmail.com>
2022-06-15 19:31:22 +0530

    Add files via upload

--- LoginLogoutTest.java
+++ LoginLogoutTest.java
@@ -5,36 +5,26 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import com.vstl.Generic.BaseTest;
-import com.vstl.PageFactory.DashboardPage;
-import com.vstl.PageFactory.DesignPage;
 import com.vstl.PageFactory.HomePage;
-import com.vstl.PageFactory.LoginPage;
 import com.vstl.PageFactory.PortfolioPage;
 import com.vstl.flows.DashboardFlow;
 import com.vstl.flows.DesignFlow;
 import com.vstl.flows.LoginLogoutFlow;
 
-import bsh.Primitive;
-
 public class LoginLogoutTest extends BaseTest {
 
-	private LoginPage objLoginPage;
+	
 	private LoginLogoutFlow objLoginLogoutFlow;
 	private HomePage objHomePage;
-	private DashboardPage objDashboardPage;
 	private DashboardFlow objDashboardFlow;
 	private PortfolioPage objPortfolioPage;
-	private DesignPage objDesignPage;
 	private DesignFlow objDesignFlow;
 
 	public void initializeWebPages() {
-		objLoginPage = new LoginPage(this);
 		objLoginLogoutFlow = new LoginLogoutFlow(this);
 		objHomePage = new HomePage(this);
-		objDashboardPage = new DashboardPage(this);
 		objDashboardFlow = new DashboardFlow(this);
 		objPortfolioPage = new PortfolioPage(this);
-		objDesignPage = new DesignPage(this);
 		objDesignFlow = new DesignFlow(this);
 	}
 
@@ -53,7 +43,7 @@ public class LoginLogoutTest extends BaseTest {
 	@Test
 	public void TCID_001_VerifyHomePageDisplayed() {
 
-		objLoginLogoutFlow.doLogin();
+		objLoginLogoutFlow.doLogin(); 
 		objHomePage.verifyHomeTextisDisplayed();
 	}
 
